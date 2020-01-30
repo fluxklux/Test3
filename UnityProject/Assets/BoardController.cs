47d5c17b11e8270ccb6cfa645b9bc759d5781be8
Alexander Vikberg <downtobusinessalex@gmail.com>
2020-01-30 12:56:55 +0100

    Added GameController

---
+++ UnityProject/Assets/BoardController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        callout();
+    }
+
+    void callout()
+    {
+        Debug.Log("Loggar från callout-funktion i BoarController");
+    }
+}
